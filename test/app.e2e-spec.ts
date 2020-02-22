f0f5b1a417097e47c4cf7a79d7f8981d2deffcd2
Chris Michael <chris.perez@upr.edu>
2020-02-22 18:08:44 -0400

    res.body.movies fixed to res.body.genres

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -20,104 +20,92 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
    
-  it('GET Method /v1/GetAllMovies/:[page]' , ok =>{
-    chai
+  it('GET Method /v1/GetAllMovies/:[page]' , async ok =>{
+    const res = await chai
       .request(app.getHttpServer())
       .get('/GetAllMovies/1')
-      .end((err, res) =>{
         expect(res).to.have.status(200);
         expect(res.body.movies).to.be.an('array');
-      })
       ok();
   });
-  it('GET Method /v1/GetLatestMovies/:[page]' , ok =>{
-    chai
+  it('GET Method /v1/GetLatestMovies/:[page]' , async ok =>{
+    const res = await chai
       .request(app.getHttpServer())
       .get('/GetLatestMovies/1')
-      .end((err, res) =>{
         expect(res).to.have.status(200);
         expect(res.body.movies).to.be.an('array');
-      })
       ok();
   });
-  it('GET Method /v1/GetPopularMovies/:[page]' , ok =>{
-    chai
+  it('GET Method /v1/GetPopularMovies/:[page]' , async ok =>{
+    const res = await chai
       .request(app.getHttpServer())
       .get('/GetPopularMovies/1')
-      .end((err, res) =>{
         expect(res).to.have.status(200);
         expect(res.body.movies).to.be.an('array');
-      })
       ok();
   });
-  it('GET Method /v1/MoviesByGenre/:[genre]/:[page]' , ok =>{
-    chai
+  it('GET Method /v1/MoviesByGenre/:[genre]/:[page]' , async ok =>{
+    const res = await chai
       .request(app.getHttpServer())
       .get('/MoviesByGenre/accion/1')
-      .end((err, res) =>{
         expect(res).to.have.status(200);
-        expect(res.body.movies).to.be.an('array');
-      })
+        expect(res.body.genres).to.be.an('array');
       ok();
   });
-  it('GET Method /v1/GetMovieServers/:[id]/:[season]' , ok =>{
-    chai
+  it('GET Method /v1/GetMovieServers/:[id]/:[season]' , async ok =>{
+    const res = await chai
       .request(app.getHttpServer())
       .get('/GetMovieServers/pelicula/frozen-ii')
-      .end((err, res) =>{
         expect(res).to.have.status(200);
         expect(res.body.servers).to.be.an('array');
-      })
       ok();
   });
-  it('GET Method /v1/GetAllSeries/:[page]' , ok =>{
-    chai
+  it('GET Method /v1/GetAllSeries/:[page]' , async ok =>{
+    const res = await chai
       .request(app.getHttpServer())
       .get('/GetAllSeries/1')
-      .end((err, res) =>{
         expect(res).to.have.status(200);
         expect(res.body.series).to.be.an('array');
-      })
       ok();
   });
-  it('GET Method /v1/GetLatestSeries/:[page]' , ok =>{
-    chai
+  it('GET Method /v1/GetLatestSeries/:[page]' , async ok =>{
+    const res = await chai
       .request(app.getHttpServer())
       .get('/GetLatestSeries/1')
-      .end((err, res) =>{
         expect(res).to.have.status(200);
         expect(res.body.series).to.be.an('array');
-      })
       ok();
   });
-  it('GET Method /v1/GetPopularSeries/:[page]' , ok =>{
-    chai
+  it('GET Method /v1/GetPopularSeries/:[page]' , async ok =>{
+    const res = await chai
       .request(app.getHttpServer())
       .get('/GetPopularSeries/1')
-      .end((err, res) =>{
         expect(res).to.have.status(200);
         expect(res.body.series).to.be.an('array');
-      })
       ok();
   });
-  it('GET Method /v1/SeriesByGenre/:[genre]/:[page]' , ok =>{
-    chai
+  it('GET Method /v1/SeriesByGenre/:[genre]/:[page]' , async ok =>{
+    const res = await chai
       .request(app.getHttpServer())
       .get('/SeriesByGenre/accion/1')
-      .end((err, res) =>{
         expect(res).to.have.status(200);
-        expect(res.body.series).to.be.an('array');
-      })
+        expect(res.body.genres).to.be.an('array');
       ok();
   });
-  it('GET Method /v1/GetAllServersBySeason/:[id]/:[season]' , ok =>{
-    chai
+  it('GET Method /v1/GetAllServersBySeason/:[id]/:[season]' , async ok =>{
+    const res = await chai
       .request(app.getHttpServer())
       .get('/GetAllServersBySeason/serie/game-of-thrones/8')
-      .end((err, res) =>{
         expect(res).to.have.status(200);
         expect(res.body.servers).to.be.an('array');
-      })
+      ok();
+  });
+  it('GET Method /v1/Search/:[query]' , async ok =>{
+    const res = await chai
+      .request(app.getHttpServer())
+      .get('/Search/game of throne')
+        expect(res).to.have.status(200);
+        expect(res.body.search).to.be.an('array');
       ok();
   });
 });
